9db382d25a44e9329ef3a3f14de878d84d54dcff
jvcmtr <jvictorcmtr@gmail.com>
2025-06-13 19:54:05 -0300

    divided App into diferent files

---
+++ app/EmailService.java
@@ -0,0 +1,8 @@
+package app;
+
+
+public class EmailService {
+    public static void sendEmail(String to, String message) {
+        System.out.println("Enviando e-mail para " + to + ": " + message);
+    }
+}
