7174d23ce314ede1b27aea8d9e3e465036e6a8d1
jvcmtr <jvictorcmtr@gmail.com>
2025-06-13 21:58:46 -0300

    Added construcotrs

--- app/Client.java
+++ app/Client.java
@@ -4,6 +4,11 @@ public class Client {
     private String name;
     private String email;
 
+    public Client(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
     public String getName() {
         return name;
     }
--- app/Product.java
+++ app/Product.java
@@ -4,6 +4,14 @@ public class Product {
     private String name;
     private Double price;
 
+    public Product(String name, Double price){
+        if (price <= 0){
+            throw new IllegalArgumentException("O Preço de um produto não pode ser igual ou menor que zero");
+        }
+        this.name = name;
+        this.price = price;
+    }
+
     public String getName() {
         return name;
     }
